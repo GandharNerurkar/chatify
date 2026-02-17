5c54163ad382359fd497fcd999dd0e8982578ae2
Gandhar Nerurkar <gandharnerurkar6@gmail.com>
2026-02-17 21:39:30 +0530

    socketStore changes

--- chatify-master/frontend/src/store/useAuthStore.js
+++ chatify-master/frontend/src/store/useAuthStore.js
@@ -6,7 +6,8 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : import.meta.env.VITE_SOCKET_URL;
+    : "https://chatify-n2ce.onrender.com";
+
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
